aa90c9e9b0ab9e48eeceed48fcc266cb2ebffd9d
Vytautas Tumas <vytautas.lsp@gmail.com>
2021-10-15 08:58:56 +0300

    Added dropdown for cities in StoreList view, which gives particular car table according to selected city. By defaut appears table of the first element in the stores list. DeleteCar action is moved to new HttpClientCar controller

---
+++ CarStoreApiConsumer/Controllers/HttpClientCarController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CarStoreApiConsumer.Controllers
+{
+    public class HttpClientCarController : Controller
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteCar(Guid storeId, Guid carId)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.DeleteAsync("https://localhost:44372/stores/" + storeId + "/cars/" + carId))
+                {
+
+                }
+            }
+            return Redirect("/HttpClientStore/StoreList");
+        }
+    }
+}
---
+++ CarStoreApiConsumer/Controllers/HttpClientStoreController.cs
@@ -0,0 +1,78 @@
+﻿using CarStoreApiConsumer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarStoreApiConsumer.Controllers
+{
+    public class HttpClientStoreController : Controller
+    {
+        public async Task<IActionResult> StoreList(Guid storeId = default)
+        {
+            List<Store> storeList = new List<Store>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:44372/stores/"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    storeList = JsonConvert.DeserializeObject<List<Store>>(apiResponse);
+                }
+            }
+            var id = storeId != Guid.Empty ? storeId : (from store in storeList select store.Id).First();
+            ViewBag.storeId = id;
+            return View(storeList);
+        }
+
+
+        public IActionResult AddStore()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddStore(Store store, Guid storeID)
+        {
+            Store receivedStore = new Store();
+            if (store.Id == Guid.Empty && storeID == Guid.Empty)
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    StringContent stringContent = new StringContent(JsonConvert.SerializeObject(store),
+                                                                    encoding: Encoding.UTF8,
+                                                                    mediaType: "application/json");
+                    using (var response = await httpClient.PostAsync("https://localhost:44372/stores/", stringContent))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        receivedStore = JsonConvert.DeserializeObject<Store>(apiResponse);
+                        ViewBag.StatusCode = (int)response.StatusCode;
+                    }
+                }
+            }
+            else
+            {
+                store.Id = storeID;
+                using (var httpClient = new HttpClient())
+                {
+                    StringContent stringContent = new StringContent(JsonConvert.SerializeObject(store),
+                                                                    encoding: Encoding.UTF8,
+                                                                    mediaType: "application/json");
+                        using (var response = await httpClient.PutAsync("https://localhost:44372/stores/", stringContent))
+                        {
+                            string apiResponse = await response.Content.ReadAsStringAsync();
+                            receivedStore = JsonConvert.DeserializeObject<Store>(apiResponse);
+                        }
+                }
+            }
+            ViewBag.Id = receivedStore.Id != Guid.Empty ? receivedStore.Id : storeID;
+            return View(receivedStore);
+        }
+
+    }
+}
